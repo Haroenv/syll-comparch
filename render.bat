41a0872a33738a0f4ad625dac3f22740110753ab
Haroenv <fingebimus@me.com>
2015-01-15 09:45:52 +0100

    to comparch

--- render.bat
+++ render.bat
@@ -1,11 +1,11 @@
 echo ...omzetting naar html
-call asciidoctor "c:/syll-comparch/book_pcarch.asc" -o "c:/syll-comparch/pcarch.html" 
+call asciidoctor "c:/syll-comparch/book_comparch.asc" -o "c:/syll-comparch/comparch.html" 
 
 ; -a stylesheet="rocket-panda.css" -a linkcss
 echo ...omzetting naar html: done!
 
 echo PUBLISH TO GHDOCS
- copy  c:\syll-comparch\pcarch.html c:\syll-comparch-ghpages\syll-comparch\html5\pcarch.html
+ copy  c:\syll-comparch\comparch.html c:\syll-comparch-ghpages\syll-comparch\html5\comparch.html
  robocopy c:\syll-comparch\ch01 c:\syll-comparch-ghpages\syll-comparch\html5\ch01 *.png *.jpg *.jpeg /mir
  robocopy c:\syll-comparch\ch02 c:\syll-comparch-ghpages\syll-comparch\html5\ch02 *.png *.jpg *.jpeg /mir 
  robocopy c:\syll-comparch\ch03 c:\syll-comparch-ghpages\syll-comparch\html5\ch03 *.png *.jpg *.jpeg /mir
@@ -15,11 +15,11 @@ echo PUBLISH TO GHDOCS
  robocopy c:\syll-comparch\ch07 c:\syll-comparch-ghpages\syll-comparch\html5\ch07 *.png *.jpg *.jpeg /mir
 
  echo ...omzetting naar docbook
- call asciidoctor -b docbook45 "c:/syll-comparch/book_pcarch.asc" -o "c:/syll-comparch/pcarch.xml" 
+ call asciidoctor -b docbook "c:/syll-comparch/book_comparch.asc" -o "c:/syll-comparch/comparch.xml" 
 echo ...omzetting naar docbook:done!
  echo ...omzetting naar PDF
- c:\fopub\asciidoctor-fopub\fopub c:\syll-comparch\pcarch.xml
- copy c:\syll-comparch\pcarch.pdf c:\syll-comparch-ghpages\syll-comparch\pdf\pcarch.pdf
+ c:\fopub\asciidoctor-fopub\fopub c:\syll-comparch\comparch.xml
+ copy c:\syll-comparch\comparch.pdf c:\syll-comparch-ghpages\syll-comparch\pdf\comparch.pdf
  echo ... volledig klaar!
 
  sleep 5
